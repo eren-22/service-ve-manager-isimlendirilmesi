24be89c352736cc82bfd1d4e2bdb026f2a850dc2
eren-22 <erenozz@yahoo.com>
2024-05-15 10:50:36 +0300

    first_commit

---
+++ Game/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            gamerManager.Add(new Gamer
+            {
+                FirstName = "Eren",
+                LastName = "Öz",
+                BirthYear = 2001,
+                IdentityNumber = 12345678910,
+            });
+        }
+    }
+}
---
+++ Game/UserValidationManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.FirstName == "Eren" && gamer.LastName == "Öz" &&
+                gamer.BirthYear == 2001 && gamer.IdentityNumber == 12345678910)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
